088fb5463ca9c28c58bca0b44e936f0a0acfe0cc
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-08-28 00:41:10 +0600

    pagination updated

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import { BsArrowLeft } from "react-icons/bs";
+import { BiLeftArrow } from "react-icons/bi";
 import { Pagination } from "react-pagination-bar";
 import "react-pagination-bar/dist/index.css";
 import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 import "./Shop.css";
 import SingleProduct from "../SingleProduct/SingleProduct";
+import logo from "../../assets/logo.png";
+import { Spinner } from "react-rainbow-components";
 
 const Shop = () => {
   const products = useLoaderData();
 
+  const [isPageLoading, setIsPageLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const pagePostsLimit = 6;
+
+  // handle page change
+  const handlePageChange = (pageNumber) => {
+    setIsPageLoading(true);
+    setTimeout(() => {
+      setCurrentPage(pageNumber);
+      setIsPageLoading(false);
+    }, 1000);
+  };
 
   console.log(products);
 
@@ -44,22 +57,37 @@ const Shop = () => {
         <h1 className="fw-bold ms-4 mt-5" style={{ color: "rgb(0, 56, 62)" }}>
           Top Selling Products of All Time
         </h1>
-        <div className="products-container container mt-5">
-          {products
-            ?.slice(
-              (currentPage - 1) * pagePostsLimit,
-              currentPage * pagePostsLimit
-            )
-            .map((product) => (
-              <div key={product.id}>
-                <SingleProduct product={product} />
-              </div>
-            ))}
-        </div>
+
+        {isPageLoading ? (
+          <div className="vh-100 d-flex justify-content-center align-items-center">
+            <div className="rainbow-position_relative rainbow-p-vertical_xx-large">
+              <Spinner size="x-large" type="arc" variant="brand">
+                <img src={logo} alt="" style={{ width: "60%" }} />
+              </Spinner>
+            </div>
+          </div>
+        ) : (
+          <>
+            {" "}
+            <div className="products-container container mt-5">
+              {products
+                ?.slice(
+                  (currentPage - 1) * pagePostsLimit,
+                  currentPage * pagePostsLimit
+                )
+                .map((product) => (
+                  <div key={product.id}>
+                    <SingleProduct product={product} />
+                  </div>
+                ))}
+            </div>
+          </>
+        )}
+
         <Pagination
           currentPage={currentPage}
           itemsPerPage={pagePostsLimit}
-          onPageChange={(pageNumber) => setCurrentPage(pageNumber)}
+          onPageChange={(pageNumber) => handlePageChange(pageNumber)}
           totalItems={products.length}
           pageNeighbours={2}
           withProgressBar={true}
